39e30179305237e5c05396f50c4ef51e88c61ac2
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-04-17 08:52:31 +0700

    Update ws-nontls.py

--- websocket/ws-nontls.py
+++ websocket/ws-nontls.py
@@ -173,14 +173,6 @@ class ConnectionHandler(threading.Thread):
             if self.method=='CONNECT':
                 port = 443
             else:
-                port = 2053
-                port = 2083
-                port = 2096
-                port = 3128
-                port = 636
-                port = 8080
-                port = 3000
-                port = 3268
                 port = sys.argv[1]
 
         (soc_family, soc_type, proto, _, address) = socket.getaddrinfo(host, port)[0]
