bbe1fb12d4488e623f00709ff114a8cb412f03f5
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-04-17 07:31:31 +0700

    Update about.sh

--- ssh/about.sh
+++ ssh/about.sh
@@ -4,7 +4,7 @@ clear
 echo -e "\033[1;31m═══════════════════════════════════════════════════\033[0m"
 echo -e "\E[44;46m    🔰 Luxury Edition by wisnu cokro satrio 🔰     \e[m "
 echo -e "\033[1;31m═══════════════════════════════════════════════════\033[0m"
-echo -e "\e[1;31m   🔰 For Debian 9 & 10 64 bit                   \e[m"
+echo -e "\e[1;31m   🔰 For Debian 9 & 10 64 bit                    \e[m"
 echo -e "\e[1;31m   🔰 For Ubuntu 18.04 & Ubuntu 20.04 64bit       \e[m"
 echo -e "\e[1;31m   🔰 For VPS with KVM and VMWare virtualization  \e[m"
 echo -e "\e[1;31m   🔰 Build Up By wisnu & gandring                \e[m"
