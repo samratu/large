90af9a323e79f17837ef6eb995845af7e9b58fbc
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-04-18 02:21:08 +0700

    Update cf.sh

--- ssh/cf.sh
+++ ssh/cf.sh
@@ -17,9 +17,9 @@ MYIP=$(wget -qO- ipinfo.io/ip);
 #MYIP=$(wget -qO- https://ipv6.icanhazip.com);
 clear
 apt install jq curl -y
-DOMAIN=gandring.my.id
+DOMAIN=zerossl.my.id
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c3)
-SUB_DOMAIN=${sub}.gandring.my.id
+SUB_DOMAIN=${sub}.zerossl.my.id
 #SUB_DOMAIN=xxx.smule.my.id
 CF_ID=djarumpentol01@gmail.com
 CF_KEY=d50aff2305f86e8f90907cfc4833ab091c375
