b94126ef9d80e523ef47f3a32d52264029e0efa6
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-04-20 11:09:58 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -299,13 +299,8 @@ chmod 644 /etc/stunnel5
 
 domain=$(cat /root/domain)
 # // Certificate File
-path_crt1="/etc/xray/xray.crt"
-path_key2="/etc/xray/xray.key"
-domain_ecc=$(cat /root/.acme.sh)
-domain.key=$(cat /root/.acme.sh/$domain_ecc)
-path_crt=/root/.acme.sh/$domain_ecc/fullchain.cer
-path_key=/root/.acme.sh/$domain_ecc/$domain.key
-cat /root/.acme.sh/$domain_ecc/$domain.key /root/.acme.sh/$domain_ecc/fullchain.cer >> /etc/stunnel5/stunnel5.pem
+path_crt="/etc/xray/xray.cer"
+path_key="/etc/xray/xray.key"
 # Download Config Stunnel5
 cat > /etc/stunnel5/stunnel5.conf <<-END
 cert = /etc/stunnel5/stunnel5.pem
@@ -333,10 +328,10 @@ connect = 127.0.0.1:1194
 END
 
 # make a certificate
-#openssl genrsa -out key.pem 1024
-#openssl req -new -x509 -key key.pem -out cert.pem -days 1095 \
-#-subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
-#cat key.pem cert.pem >> /etc/stunnel5/stunnel5.pem
+openssl genrsa -out key.pem 1024
+openssl req -new -x509 -key key.pem -out cert.pem -days 1095 \
+-subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
+cat key.pem cert.pem >> /etc/stunnel5/stunnel5.pem
 
 # Service Stunnel5 systemctl restart stunnel5
 cat > /etc/systemd/system/stunnel5.service << END
