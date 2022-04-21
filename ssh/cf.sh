447747e6fa0c766c9f80d72d85a05ba25d1a6d46
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-04-22 03:15:19 +0700

    Update cf.sh

--- ssh/cf.sh
+++ ssh/cf.sh
@@ -14,7 +14,7 @@ LIGHT='\033[0;37m'
 # Getting
 #MYIP=$(wget -qO- ipinfo.io/ip);
 MYIP=$(wget -qO- https://ipv4.icanhazip.com);
-MYIP=$(wget -qO- https://ipv6.icanhazip.com);
+MYIP6=$(wget -qO- https://ipv6.icanhazip.com);
 clear
 apt install jq curl -y
 DOMAIN=smule.my.id
