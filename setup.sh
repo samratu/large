b759278035348904714429c5a6c912953cb60e39
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-04-27 02:38:53 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -113,7 +113,7 @@ echo " 🔰 PORT DROPBEAR : 200, 300" |tee -a log-install.txt
 echo " 🔰 PORT SQUID : 3128, 3228" |tee -a log-install.txt
 echo " 🔰 Badvpn : 7100, 7200, 7300-7900" |tee -a log-install.txt
 echo " 🔰 Nginx : 88" |tee -a log-install.txt
-echo " 🔰 Wireguard : 8080" |tee -a log-install.txt
+echo " 🔰 Wireguard : 591" |tee -a log-install.txt
 echo " 🔰 L2TP/IPSEC VPN : 1701" |tee -a log-install.txt
 echo " 🔰 PPTP VPN : 1732" |tee -a log-install.txt
 echo " 🔰 SSTP VPN : 444" |tee -a log-install.txt
@@ -123,7 +123,7 @@ echo " 🔰 SS-OBFS HTTP : 2944-3442" |tee -a log-install.txt
 echo " 🔰 XRAY SHADOWSOCKS : 333" |tee -a log-install.txt
 echo " 🔰 XRAY MTPROTO : 111" |tee -a log-install.txt
 echo " 🔰 WEBSOCKET TLS : 443" |tee -a log-install.txt
-echo " 🔰 WEBSOCKET NON TLS : 8880" |tee -a log-install.txt
+echo " 🔰 WEBSOCKET NON TLS : 80" |tee -a log-install.txt
 echo " 🔰 PORT OVPNWS : 2086" |tee -a log-install.txt
 echo " 🔰 OHP SSH : 8181" |tee -a log-install.txt
 echo " 🔰 OHP Dropbear : 8282" |tee -a log-install.txt
@@ -132,25 +132,27 @@ echo " 🔰 VMESS WS TLS : 2053" |tee -a log-install.txt
 echo " 🔰 VMESS WS NON TLS : 2052" |tee -a log-install.txt
 echo " 🔰 VMESS GRPC TLS: 2053" |tee -a log-install.txt
 echo " 🔰 VMESS GRPC NON TLS: 2052" |tee -a log-install.txt
-echo " 🔰 VMESS H2C : 2053" |tee -a log-install.txt
-echo " 🔰 VMESS HTTP TLS : 8088" |tee -a log-install.txt
+echo " 🔰 VMESS H2C TLS : 3443" |tee -a log-install.txt
+echo " 🔰 VMESS H2C NON TLS : 3444" |tee -a log-install.txt
+echo " 🔰 VMESS HTTP TLS : 8808" |tee -a log-install.txt
 echo " 🔰 VMESS HTTP NON TLS : 808" |tee -a log-install.txt
 echo " 🔰 VLESS XTLS : 44443" |tee -a log-install.txt
 echo " 🔰 VLESS WS TLS : 2083" |tee -a log-install.txt
 echo " 🔰 VLESS WS NON TLS : 2082" |tee -a log-install.txt
-echo " 🔰 VLESS GRPC TLS : 8880" |tee -a log-install.txt
+echo " 🔰 VLESS GRPC TLS : 2083" |tee -a log-install.txt
 echo " 🔰 VLESS GRPC NON TLS: 2082" |tee -a log-install.txt
-echo " 🔰 VLESS H2C : 1443" |tee -a log-install.txt
-echo " 🔰 VLESS HTTP TLS : 8088" |tee -a log-install.txt
+echo " 🔰 VLESS H2C TLS : 1443" |tee -a log-install.txt
+echo " 🔰 VLESS H2C NON TLS : 1444" |tee -a log-install.txt
+echo " 🔰 VLESS HTTP TLS : 8888" |tee -a log-install.txt
 echo " 🔰 VLESS HTTP NON TLS : 888" |tee -a log-install.txt
 echo " 🔰 TROJAN GO : 2087" |tee -a log-install.txt
 echo " 🔰 TROJAN GRPC : 8443" |tee -a log-install.txt
 echo " 🔰 TROJAN HTTP : 880" |tee -a log-install.txt
-echo " 🔰 TROJAN H2C : 8808" |tee -a log-install.txt
+echo " 🔰 TROJAN H2C : 8880" |tee -a log-install.txt
 echo " 🔰 TROJAN XTLS : 4443" |tee -a log-install.txt
 echo " 🔰 TROJAN WS TLS : 2096" |tee -a log-install.txt
 echo " 🔰 TROJAN WS NON TLS : 2095" |tee -a log-install.txt
-echo " 🔰 SOCKS5 WEBSOCKET : 1080" |tee -a log-install.txt
+echo " 🔰 SOCKS5 WEBSOCKET : 777" |tee -a log-install.txt
 echo " 🔰 XRAY SHADOWSOCKS : 333" |tee -a log-install.txt
 echo " 🔰 XRAY MT PROTO : 111" |tee -a log-install.txt
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
