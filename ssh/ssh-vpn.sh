8e8c27054d16c363bbdb5ee6e802ce8bf15ad005
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-04-27 12:21:33 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -325,8 +325,8 @@ cat key.pem cert.pem >> /etc/stunnel5/stunnel5.pem
 # Download Config Stunnel5
 #cat > /etc/stunnel5/stunnel5.conf <<-END
 cert = /etc/stunnel5/stunnel5.pem
-#key = /etc/xray/xray.key
-#cert = /etc/xray/xray.cer
+key = /etc/xray/xray.key
+cert = /etc/xray/xray.cer
 
 client = no
 socket = a:SO_REUSEADDR=1
@@ -616,7 +616,7 @@ chmod +x addnewvmess
 chmod +x addnewvless
 chmod +x delnewtr
 chmod +x delnewvmess
-chmod +x addnewvless
+chmod +x delnewvless
 chmod +x restart
 chmod +x portdropbear
 chmod +x portopenssh
