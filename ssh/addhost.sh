c8b01f78ce5e2162910f936a2fa9e568a219575a
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-04-28 14:17:27 +0700

    Update addhost.sh

--- ssh/addhost.sh
+++ ssh/addhost.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-MYIP=$(wget -qO- https://icanhazip.com);
-DOMAIN=$(wget -qO- https://icanhazip.com);
-MYIP=$domain
-read -p "Masukan Subdomain Anda :" sub
-curl https://get.acme.sh | sh
-alias acme.sh=~/.acme.sh/acme.sh
-/root/.acme.sh/acme.sh --upgrade --auto-upgrade
-/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
-/root/.acme.sh/acme.sh --issue -d "${domain}" --standalone --keylength ec-384
-/root/.acme.sh/acme.sh --install-cert -d "${domain}" --ecc \
---fullchain-file /etc/xray/xray.crt \
---key-file /etc/xray/xray.key
-chown -R nobody:nogroup /etc/xray
-chmod 644 /etc/xray/xray.crt
-chmod 644 /etc/xray/xray.key
+echo "Tukar Domain?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes) make install ; break ;;
+        No) exit ;;
+    esac
+done
+clear
+
+echo -e "Masukkan Domain"
+read -p "Hostname / Domain: " host
+rm -f /var/lib/wisnuvs/ipvps.conf
+rm -f /etc/xray/domain
+clear
+mkdir /etc/xray
+mkdir /var/lib/wisnucs;
+clear
+echo -e "Masukkan Domain Sekali Lagi"
+read -p "Hostname / Domain: " host
+echo "IP=$host" >> /var/lib/wisnucs/ipvps.conf
+echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
+echo "$host" >> /etc/xray/domain
