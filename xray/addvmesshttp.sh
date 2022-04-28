f856ca75b636af3ea2612dcc7352beb56bce0e20
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-04-29 04:58:26 +0700

    Update addvmesshttp.sh

--- xray/addvmesshttp.sh
+++ xray/addvmesshttp.sh
@@ -6,7 +6,9 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 
-MYIP=$(wget -qO- ipinfo.io/ip);
+#MYIP=$(wget -qO- ipinfo.io/ip);
+MYIP=$(wget -qO- https://ipv4.icanhazip.com);
+MYIP6=$(wget -qO- https://ipv6.icanhazip.com);
 clear
 domain=$(cat /etc/xray/domain)
 vmhttp="$(cat ~/log-install.txt | grep -w "VMESS HTTP TLS" | cut -d: -f2|sed 's/ //g')"
@@ -70,18 +72,19 @@ echo -e ""
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "\033[1;46m  🔰 AKUN VMESS HTTP 🔰   \e[m"   
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Remarks : ${user}"
-echo -e "Host : ${domain}"
-echo -e "IP : ${MYIP}"
-echo -e "Port TLS : ${vmhttp}"
-echo -e "Port NON TLS : ${vmhttpnon}"
-echo -e "Id : ${uuid}"
-echo -e "AlterId : 0"
-echo -e "Security : auto"
-echo -e "Network : tcp"
-echo -e "Host : ${domain}"
-#echo -e "Path : /"
-echo -e "Expired On : $exp"
+echo -e "Remarks      :${user}"
+echo -e "Host         :${domain}"
+echo -e "IP           :${MYIP}"
+echo -e "IPV6         :$MYIP6"
+echo -e "Port TLS     :${vmhttp}"
+echo -e "Port NON TLS :${vmhttpnon}"
+echo -e "Id           :${uuid}"
+echo -e "AlterId      :0"
+echo -e "Security     :auto"
+echo -e "Network      :tcp"
+echo -e "Host         :${domain}"
+#echo -e "Path        :/"
+echo -e "Expired On   :$exp"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "HTTP TLS : ${vmesshttp}"   
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
