d0825082535b419ff6e5ddc2eb4ab0d072ace225
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-04-29 02:33:04 +0700

    Update addnewvless.sh

--- xray/addnewvless.sh
+++ xray/addnewvless.sh
@@ -47,6 +47,7 @@ until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
 	done
 uuid=$(cat /proc/sys/kernel/random/uuid)
 read -p "Expired (Days) : " masaaktif
+read -p "Expired (Seconds) : " masaaktif
 hariini=`date -d "0 days" +"%Y-%m-%d"`
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 exp2=`date -d "$masaaktif seconds" +"%Y-%m-%d"`
