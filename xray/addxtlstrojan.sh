76de6a25100a0b3c95b91667a7bffa60899f5d26
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-04-29 04:59:50 +0700

    Update addxtlstrojan.sh

--- xray/addxtlstrojan.sh
+++ xray/addxtlstrojan.sh
@@ -12,7 +12,9 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 # Getting
-MYIP=$(wget -qO- ipinfo.io/ip);
+#MYIP=$(wget -qO- ipinfo.io/ip);
+MYIP=$(wget -qO- https://ipv4.icanhazip.com);
+MYIP6=$(wget -qO- https://ipv6.icanhazip.com);
 clear
 domain=$(cat /etc/xray/domain)
 
@@ -45,16 +47,17 @@ echo -e ""
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "\033[1;46m 🔰 AKUN TROJAN XTLS 🔰   \e[m"       
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Remarks  : ${user}"
-echo -e "IP/Host  : ${MYIP}"
-echo -e "Address  : ${domain}"
-echo -e "Protocol : tcp"
+echo -e "Remarks  :${user}"
+echo -e "IP/Host  :${MYIP}"
+echo -e "IPV6     :$MYIP6"
+echo -e "Address  :${domain}"
+echo -e "Protocol :tcp"
 #echo -e "ServiceName: gandring"
-echo -e "Port     : ${txtls}"
-echo -e "Password : ${uuid}"
-echo -e "Security : XTLS/TLS"
-echo -e "Created  : $hariini"
-echo -e "Expired  : $exp"
+echo -e "Port     :${txtls}"
+echo -e "Password :${uuid}"
+echo -e "Security :XTLS/TLS"
+echo -e "Created  :$hariini"
+echo -e "Expired  :$exp"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "XTLS : ${trojanxtls}"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
