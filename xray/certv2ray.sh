8d824b601c55de1166db293ca5179efa759f421a
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-04-30 06:13:39 +0700

    Update certv2ray.sh

--- xray/certv2ray.sh
+++ xray/certv2ray.sh
@@ -2,16 +2,16 @@
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
-MYIP=$(wget -qO- https://icanhazip.com);
+MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
 clear
 echo start
 sleep 0.5
-source /var/lib/akbarstorevpn/ipvps.conf
+source /var/lib/wisnucs/ipvps.conf
 domain=$IP
 systemctl stop xray
 
 /root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
-~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.cer --keypath /etc/xray/xray.key --ecc
 systemctl start xray
 echo Done
