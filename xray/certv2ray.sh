81df0d41598c64ff88c408b35a82d9ee709b87c2
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-04-30 06:15:26 +0700

    Update certv2ray.sh

--- xray/certv2ray.sh
+++ xray/certv2ray.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-clear
 echo start
 sleep 0.5
 source /var/lib/wisnucs/ipvps.conf
 domain=$IP
+systemctl stop xtrojan
 systemctl stop xray
-
 /root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
 ~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.cer --keypath /etc/xray/xray.key --ecc
 systemctl start xray
+systemctl start xtrojan
+systemctl start xvless
 echo Done
+sleep 0.5
+clear 
+status
