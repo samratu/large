feca017eeefae9ad2e772ed1bdfc7d0240e0aecf
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-05-22 17:31:20 +0700

    Update certv2ray.sh

--- xray/certv2ray.sh
+++ xray/certv2ray.sh
@@ -5,7 +5,6 @@ source /var/lib/wisnucs/ipvps.conf
 domain=$IP
 systemctl stop xtrojan
 systemctl stop xray
-sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
 /root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
 ~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.cer --keypath /etc/xray/xray.key --ecc
 systemctl start xray
