3d30055ed6ac7a0358e89e59625234776a8e7336
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-05-22 14:26:54 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -140,13 +140,13 @@ echo " 🔰 VLESS WS TLS : 8443" |tee -a log-install.txt
 echo " 🔰 VLESS WS NON TLS : 8880" |tee -a log-install.txt
 echo " 🔰 VLESS GRPC TLS : 2083" |tee -a log-install.txt
 echo " 🔰 VLESS GRPC NON TLS: 2082" |tee -a log-install.txt
-echo " 🔰 VLESS H2C : 5443" |tee -a log-install.txt
+echo " 🔰 VLESS H2C : 441" |tee -a log-install.txt
 echo " 🔰 VLESS HTTP TLS : 8088" |tee -a log-install.txt
 echo " 🔰 VLESS HTTP NON TLS : 888" |tee -a log-install.txt
 echo " 🔰 TROJAN GO : 2086" |tee -a log-install.txt
 echo " 🔰 TROJAN GRPC : 2096" |tee -a log-install.txt
 echo " 🔰 TROJAN HTTP : 880" |tee -a log-install.txt
-echo " 🔰 TROJAN H2C : 3443" |tee -a log-install.txt
+echo " 🔰 TROJAN H2C : 442" |tee -a log-install.txt
 echo " 🔰 TROJAN XTLS : 4443" |tee -a log-install.txt
 echo " 🔰 TROJAN WS TLS : 443" |tee -a log-install.txt
 echo " 🔰 TROJAN WS NON TLS : 2095" |tee -a log-install.txt
