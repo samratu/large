261d4ccf4af9c433a65a104adc06eea47e3c7baf
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-05-28 12:18:58 +0700

    Update certv2ray.sh

--- xray/certv2ray.sh
+++ xray/certv2ray.sh
@@ -5,8 +5,20 @@ source /var/lib/wisnucs/ipvps.conf
 domain=$IP
 systemctl stop xtrojan
 systemctl stop xray
-/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
-~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.cer --keypath /etc/xray/xray.key --ecc
+
+sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
+##Generate acme certificate
+curl https://get.acme.sh | sh
+alias acme.sh=~/.acme.sh/acme.sh
+/root/.acme.sh/acme.sh --upgrade --auto-upgrade
+/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+/root/.acme.sh/acme.sh --issue -d "${domain}" --standalone --keylength ec-256
+/root/.acme.sh/acme.sh --install-cert -d "${domain}" --ecc \
+--fullchain-file /etc/xray/xray.cer \
+--key-file /etc/xray/xray.key
+chown -R nobody:nogroup /etc/xray
+chmod 644 /etc/xray/xray.cer
+chmod 644 /etc/xray/xray.key
 systemctl start xray
 systemctl start xtrojan
 systemctl start xvless
