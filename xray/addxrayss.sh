b957ab3db58f2c233a45c006a01ace432f8db77a
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-05-31 05:43:10 +0700

    Update addxrayss.sh

--- xray/addxrayss.sh
+++ xray/addxrayss.sh
@@ -15,10 +15,13 @@ LIGHT='\033[0;37m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 clear
 domain=$(cat /etc/xray/domain)
-#tls="$(cat ~/log-install.txt | grep -w "XRAY SHADOWSOCKS" | cut -d: -f2|sed 's/ //g')"
-ss="$(cat ~/log-install.txt | grep -w "XRAY SHADOWSOCKS" | cut -d: -f2|sed 's/ //g')"
+sstls="$(cat ~/log-install.txt | grep -w "SHADOWSOCKS WS TLS" | cut -d: -f2|sed 's/ //g')"
+sstcp="$(cat ~/log-install.txt | grep -w "SHADOWSOCKS TCP" | cut -d: -f2|sed 's/ //g')"
+ssnontls="$(cat ~/log-install.txt | grep -w "SHADOWSOCKS WS NON TLS" | cut -d: -f2|sed 's/ //g')"
+ssudp="$(cat ~/log-install.txt | grep -w "SHADOWSOCKS UDP" | cut -d: -f2|sed 's/ //g')"
+ssnew="$(cat ~/log-install.txt | grep -w "SHADOWSOCKS 2022" | cut -d: -f2|sed 's/ //g')"
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
-		read -rp "Username : " -e user
+		read -rp "Password : " -e user
 		CLIENT_EXISTS=$(grep -w $user /etc/xray/config.json | wc -l)
 
 		if [[ ${CLIENT_EXISTS} == '1' ]]; then
@@ -31,27 +34,26 @@ uuid=$(cat /proc/sys/kernel/random/uuid)
 read -p "Expired (Days) : " masaaktif
 hariini=`date -d "0 days" +"%Y-%m-%d"`
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
-#sed -i '/#vmess-tls$/a\### '"$user $exp"'\
-#},{"id": "'""$uuid""'"' /etc/xray/config.json
-sed -i '/#xray-ss$/a\### '"$user $exp"'\
-},{"id": "'""$uuid""'"' /etc/xray/config.json
-cat>/etc/xray/ss-$user.json<<EOF
+sed -i '/#xray-ss-new$/a\#&# '"$user $exp"'\
+},{"password": "'""$uuid""'","email": "'""$user""'"' /etc/xray/xss.json
+cat>/etc/xray/ss-$user-new.json<<EOF
       {
-      "port": 333,
+  "inbounds": [
+    {
+      "port": 212,
       "protocol": "shadowsocks",
       "settings": {
-        "method": "chacha20-poly1305",
+        "method": "2022-blake3-aes-128-gcm",
         "password": "${user}",
         "network": "tcp,udp"
       }
     },
 EOF
-#vmess_base641=$( base64 -w 0 <<< $shadowsocks_json)
-shadowsocks_base64=$(echo -n "chacha20-poly1305:${user}@${MYIP}:$ss" | base64 -w0)
-#vmess1="vmess://$(base64 -w 0 /etc/xray/ss-$user.json)"
-shadowsocks="ss://$shadowsocks_base64"
-
+tmp5=$(echo -n "aes-128-gcm:${user}@${domain}:$ssnew" | base64 -w0)
+shadowsocksnew="ss://$tmp5#$user"
 systemctl restart xray.service
+systemctl restart xss.service
+systemctl restart xtrojan.service
 service cron restart
 clear
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
@@ -61,18 +63,14 @@ echo -e "Remarks     : ${user}"
 echo -e "IP/Host     : ${MYIP}"
 echo -e "Address     : ${domain}"
 #echo -e "Port TLS    : ${tls}"
-echo -e "Port SS     : ${ss}"
-#echo -e "User ID     : ${uuid}"
-#echo -e "Alter ID    : 0"
-echo -e "Security    : chacha20-poly1305"
+echo -e "Port SS     : $ssnew"
+echo -e "Security    : 2022-blake3-aes-128-gcm"
 echo -e "Network     : tcp,udp"
 echo -e "Password    : ${user}"
 echo -e "Created     : $hariini"
 echo -e "Expired     : $exp"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Link SS     : ${shadowsocks}"
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-#echo -e "Link No TLS : ${vmess2}"
+echo -e "Link SS new : ${shadowsocksnew}"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "\033[1;46m  🔰LUXURY EDITION BY ZEROSSL🔰   \e[m"   
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
