2c973e7c061ba1e27ee859c6d0b3ce1ecc241c71
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-06-01 14:29:51 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -307,11 +307,11 @@ chmod 644 /etc/stunnel5
 
 domain=$(cat /root/domain)
 #openssl req -newkey rsa:2048 -nodes -keyout $domain.key -out $domain.csr
-#openssl genrsa -out key.pem 1024
-#openssl req -new -x509 -key key.pem -out cert.pem -days 1095 \
-#-subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
-#cat key.pem cert.pem >> /etc/stunnel5/stunnel5.pem
-cat /etc/xray/xray.crt /etc/xray/xray.key >> /etc/stunnel5/stunnel5.pem
+openssl genrsa -out key.pem 1024
+openssl req -new -x509 -key key.pem -out cert.pem -days 1095 \
+-subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
+cat key.pem cert.pem >> /etc/stunnel5/stunnel5.pem
+#cat /etc/xray/xray.crt /etc/xray/xray.key >> /etc/stunnel5/stunnel5.pem
 
 # Ubah Izin Akses
 chmod 755 /etc/stunnel5/stunnel5.pem
