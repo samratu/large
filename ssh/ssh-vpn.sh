c994b731fe9ce4f18c89ec33d40153b2c06ef166
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-06-03 22:00:23 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -284,9 +284,9 @@ RUN=yes
 # systemd users: don't forget to modify /lib/systemd/system/sslh.service
 #DAEMON=/usr/sbin/sslh
 
-#DAEMON_OPTS="--user sslh --listen 0.0.0.0:8443 --ssh 127.0.0.1:22 --ssl 127.0.0.1:500 --ssh 127.0.0.1:300 --openvpn 127.0.0.1:1194 --http 127.0.0.1:2082 --pidfile /var/run/sslh/sslh.pid"
+DAEMON_OPTS="--user sslh --listen 0.0.0.0:8443 --ssh 127.0.0.1:22 --ssl 127.0.0.1:500 --ssh 127.0.0.1:300 --openvpn 127.0.0.1:1194 --http 127.0.0.1:2082 --pidfile /var/run/sslh/sslh.pid"
 
-#END
+END
 
 # Restart Service SSLH
 service sslh restart
