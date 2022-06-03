a3fad2d240c831571bc498e73496de73e633517f
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-06-04 04:46:37 +0700

    Update edu.sh

--- websocket/edu.sh
+++ websocket/edu.sh
@@ -35,7 +35,7 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/ws-nontls 8280
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-nontls 2082
 Restart=on-failure
 
 [Install]
@@ -91,7 +91,7 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/ws-tls 9443
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-tls 8443
 Restart=on-failure
 
 [Install]
