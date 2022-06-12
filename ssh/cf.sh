454bdf49253cd16f6c6c5421af407a61e0d81808
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-06-12 08:20:12 +0700

    Update cf.sh

--- ssh/cf.sh
+++ ssh/cf.sh
@@ -16,8 +16,8 @@ MYIP=$(wget -qO- ipinfo.io/ip);
 clear
 apt install jq curl -y
 DOMAIN=zerossl.my.id
-sub=$(</dev/urandom tr -dc a-z0-9 | head -c2)
-SUB_DOMAIN=$sub.zerossl.my.id
+#sub=$(</dev/urandom tr -dc a-z0-9 | head -c2)
+#SUB_DOMAIN=$sub.zerossl.my.id
 CF_ID=djarumpentol01@gmail.com
 CF_KEY=d50aff2305f86e8f90907cfc4833ab091c375
 set -euo pipefail
