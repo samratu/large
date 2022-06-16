5a609d77bf95e17283b5008ac56c59047c79806e
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-06-16 10:23:05 +0700

    Update xp.sh

--- ssh/xp.sh
+++ ssh/xp.sh
@@ -204,14 +204,14 @@ systemctl start xvless.service
 systemctl enable xvless.service
 systemctl restart xvless.service
 
-exp=$(grep -w "^#### $user" "/etc/xray/xvmess.json" | cut -d ' ' -f 3)
+exp=$(grep -w "^#### $user" "/usr/local/etc/xray/xvmess.json" | cut -d ' ' -f 3)
 #exp=$(grep -w "^#### $user" "/etc/v2ray/config.json" | cut -d ' ' -f 3)
 d1=$(date -d "$exp" +%s)
 d2=$(date -d "$now" +%s)
 exp2=$(( (d1 - d2) / 86400 ))
 if [[ "$exp2" = "0" ]]; then
-sed -i "/^#### $user $exp/,/^},{/d" /etc/xray/xvmess.json
-sed -i "/^#### $user $exp/,/^},{/d" /etc/xray/xvmess.json
+sed -i "/^#### $user $exp/,/^},{/d" /usr/local/etc/xray/xvmess.json
+sed -i "/^#### $user $exp/,/^},{/d" /usr/local/etc/xray/xvmess.json
 #sed -i "/^#### $user $exp/,/^},{/d" /etc/v2ray/config.json
 #sed -i "/^#### $user $exp/,/^},{/d" /etc/v2ray/config.json
 fi
