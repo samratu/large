5cd6ad8281cc7d8fc02efe83670cc7be60d1ee9b
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-06-18 04:26:12 +0700

    Update ceknewtr.sh

--- xray/ceknewtr.sh
+++ xray/ceknewtr.sh
@@ -18,7 +18,7 @@ echo -n > /tmp/other.txt
 data=( `cat /etc/xray/xtrojan.json | grep '^#&#' | cut -d ' ' -f 2`);
 data=( `cat /usr/local/etc/xray/xvmess.json | grep '^#&#' | cut -d ' ' -f 2`);
 data=( `cat /etc/xray/xvless.json | grep '^#&#' | cut -d ' ' -f 2`);
-data=( `cat /etc/xray/xray.json | grep '^#&#' | cut -d ' ' -f 2`);
+data=( `cat /etc/xray/config.json | grep '^#&#' | cut -d ' ' -f 2`);
 echo "-----------------------------------------";
 echo "---------=[ xray User Login ]=---------";
 echo "-----------------------------------------";
