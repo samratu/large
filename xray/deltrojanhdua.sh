7f3f83a4ad179a602e30801081bb2a536d2da3ff
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-06-18 04:50:52 +0700

    Update deltrojanhdua.sh

--- xray/deltrojanhdua.sh
+++ xray/deltrojanhdua.sh
@@ -34,12 +34,37 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#&# " "/etc/xray/xtrojan.json")
 			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
 		fi
 	done
+NUMBER_OF_CLIENTS=$(grep -c -E "^#&# " "/usr/local/etc/xray/xvmess.json")
+	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+		echo ""
+		echo "You have no existing clients!"
+		exit 1
+	fi
+
+	echo ""
+	echo " Select the existing client you want to remove"
+	echo " Press CTRL+C to return"
+	echo " ==============================="
+	echo "     No  Expired   User"
+	grep -E "^#&# " "/usr/local/etc/xray/xvmess.json" | cut -d ' ' -f 2-3 | nl -s ') '
+	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
+		if [[ ${CLIENT_NUMBER} == '1' ]]; then
+			read -rp "Select one client [1]: " CLIENT_NUMBER
+		else
+			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
+		fi
+	done
 user=$(grep -E "^#&# " "/etc/xray/xtrojan.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
 exp=$(grep -E "^#&# " "/etc/xray/xtrojan.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
 sed -i "/^#&# $user $exp/,/^},{/d" /etc/xray/xtrojan.json
 sed -i "/^#&# $user $exp/,/^},{/d" /etc/xray/xtrojan.json
+user=$(grep -E "^#&# " "/usr/local/etc/xray/xvmess.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^#&# " "/usr/local/etc/xray/xvmess.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+sed -i "/^#&# $user $exp/,/^},{/d" /usr/local/etc/xray/xvmess.json
+sed -i "/^#&# $user $exp/,/^},{/d" /usr/local/etc/xray/xvmess.json
 systemctl restart xray.service
 systemctl restart xtrojan.service
+systemctl restart xvmess
 service cron restart
 clear
 echo ""
