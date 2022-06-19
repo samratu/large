6431170a5813ba6cc9f976bf36d8752d8b00fd24
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-06-19 18:41:42 +0700

    Update addnewvmess.sh

--- xray/addnewvmess.sh
+++ xray/addnewvmess.sh
@@ -57,7 +57,7 @@ cat>/etc/xray/vmess-$user-tls.json<<EOF
       "id": "${uuid}",
       "aid": "0",
       "net": "ws",
-      "path": "/satrio",
+      "path": "/shanum",
       "type": "none",
       "host": "${domain}",
       "tls": "tls"
@@ -72,7 +72,7 @@ cat>/etc/xray/vmess-$user-nontls.json<<EOF
       "id": "${uuid}",
       "aid": "0",
       "net": "ws",
-      "path": "/satrio",
+      "path": "/shanum",
       "type": "none",
       "host": "${domain}",
       "tls": "none"
@@ -137,7 +137,7 @@ cat>/etc/xray/vmess-$user-tls.json<<EOF
       "id": "${uuid}",
       "aid": "0",
       "net": "grpc",
-      "path": "/satrio",
+      "path": "/shanumgrpc",
       "type": "none",
       "host": "${domain}",
       "tls": "tls"
@@ -152,7 +152,7 @@ cat>/etc/xray/vmess-$user-nontls.json<<EOF
       "id": "${uuid}",
       "aid": "0",
       "net": "grpc",
-      "path": "/satrio",
+      "path": "/shanumgrpc",
       "type": "none",
       "host": "${domain}",
       "tls": "none"
@@ -208,7 +208,7 @@ cat>/etc/xray/vmess-$user-tls.json<<EOF
       "id": "${uuid}",
       "aid": "0",
       "net": "h2",
-      "path": "/satrio",
+      "path": "/shanumhttp",
       #"type": "multi",
       "host": "$domain",
       "tls": "tls"
@@ -262,7 +262,7 @@ cat>/etc/xray/vmess-$user-tls.json<<EOF
       "id": "${uuid}",
       "aid": "0",
       "net": "tcp",
-      "path": "/satrio",
+      "path": "/shanumtcp",
       "type": "http",
       "host": "${domain}",
       "tls": "tls"
@@ -306,7 +306,10 @@ echo -e "Port GRPC        :${vmgrpc}/${vmgrpcnon}"
 echo -e "Port H2C         :${vmhdua}"
 echo -e "Port HTTP        :${vmhttp}/${vmhttpnon}"
 echo -e "Protokol         :WS,GRPC,H2C,HTTP"
-echo -e "Path/serviceName :/cokro,/gandring"
+echo -e "Path GRPC        :/shanumgrpc"
+echo -e "Path HTTP        :/shanumgtcp"
+echo -e "Path H2C         :/shanumhttp"
+echo -e "Path WS          :/shanum"
 echo -e "Dibuat           :$hariini"
 echo -e "Kadaluarsa       :$exp"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
