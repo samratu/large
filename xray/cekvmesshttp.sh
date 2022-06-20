be4ac1fa21c7ebd515333c76e399fbb9ae0c83af
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-06-20 08:34:16 +0700

    Update cekvmesshttp.sh

--- xray/cekvmesshttp.sh
+++ xray/cekvmesshttp.sh
@@ -18,6 +18,7 @@ echo -n > /tmp/other.txt
 data=( `cat /etc/xray/config.json | grep '^###' | cut -d ' ' -f 2`);
 data=( `cat /usr/local/etc/xray/xvmess.json | grep '^#&#' | cut -d ' ' -f 2`);
 data=( `cat /etc/xray/xtrojan.json | grep '^#&#' | cut -d ' ' -f 2`);
+data=( `cat /usr/local/etc/xray/satrio.json | grep '^#&#' | cut -d ' ' -f 2`);
 echo "----------------------------------------";
 echo "---------=[ Vmess User Login ]=---------";
 echo "----------------------------------------";
