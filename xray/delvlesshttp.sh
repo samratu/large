f6d8a7fcb0466138aa73aa85683e703034a9b7dd
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-06-20 08:27:24 +0700

    Update delvlesshttp.sh

--- xray/delvlesshttp.sh
+++ xray/delvlesshttp.sh
@@ -77,6 +77,27 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/usr/local/etc/xray/xvmess.json")
 			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
 		fi
 	done
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/usr/local/etc/xray/satrio.json")
+	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+		echo ""
+		echo "You have no existing clients!"
+		exit 1
+	fi
+
+	clear
+	echo ""
+	echo " Select the existing client you want to remove"
+	echo " Press CTRL+C to return"
+	echo " ==============================="
+	echo "     No  Expired   User"
+	grep -E "^### " "/usr/local/etc/xray/satrio.json" | cut -d ' ' -f 2-3 | nl -s ') '
+	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
+		if [[ ${CLIENT_NUMBER} == '1' ]]; then
+			read -rp "Select one client [1]: " CLIENT_NUMBER
+		else
+			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
+		fi
+	done
 user=$(grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
 exp=$(grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
 sed -i "/^### $user $exp/,/^},{/d" /etc/xray/config.json
@@ -86,8 +107,14 @@ sed -i "/^### $user $exp/,/^},{/d" /etc/xray/xvless.json
 user=$(grep -E "^### " "/usr/local/etc/xray/xvmess.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
 exp=$(grep -E "^### " "/usr/local/etc/xray/xvmess.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
 sed -i "/^### $user $exp/,/^},{/d" /usr/local/etc/xray/xvmess.json
+user=$(grep -E "^### " "/usr/local/etc/xray/satrio.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^### " "/usr/local/etc/xray/satrio.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+sed -i "/^### $user $exp/,/^},{/d" /usr/local/etc/xray/satrio.json
 rm -f /etc/xray/vmess-$user-tls.json /etc/xray/vmess-$user-tls.json
 systemctl restart xray.service
+systemctl restart xvmess
+systemctl restart satrio
+
 clear
 echo ""
 echo "==============================="
