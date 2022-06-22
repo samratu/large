5b300165913b3838b7b9feae7c33d5b29b017c10
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-06-22 18:46:21 +0700

    Update certv2ray.sh

--- xray/certv2ray.sh
+++ xray/certv2ray.sh
@@ -15,13 +15,21 @@ alias acme.sh=~/.acme.sh/acme.sh
 /root/.acme.sh/acme.sh --issue -d "${domain}" --standalone --keylength ec-256
 /root/.acme.sh/acme.sh --install-cert -d "${domain}" --ecc \
 --fullchain-file /etc/xray/xray.cer \
---key-file /etc/xray/xray.key
+#--key-file /etc/xray/xray.key
+#chown -R nobody:nogroup /etc/xray
+#chmod 644 /etc/xray/xray.cer
+#chmod 644 /etc/xray/xray.key
+--fullchain-file /etc/ssl/private/fullchain.pem \
+--key-file /etc/ssl/private/privkey.pem
 chown -R nobody:nogroup /etc/xray
-chmod 644 /etc/xray/xray.cer
-chmod 644 /etc/xray/xray.key
+chmod 644 /etc/ssl/private/fullchain.pem
+chmod 644 /etc/ssl/private/privkey.pem
 systemctl start xray
 systemctl start xtrojan
 systemctl start xvless
+systemctl restart xvmess
+systemctl restart xss
+systemctl restart satrio
 echo Done
 sleep 0.5
 clear 
