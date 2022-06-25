38521f7762fb9eedf769e02ecffd9dc411931aa8
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-06-25 11:43:38 +0700

    Update addnewtr.sh

--- xray/addnewtr.sh
+++ xray/addnewtr.sh
@@ -18,6 +18,7 @@ clear
 domain=$(cat /etc/xray/domain)
 
 uuid=$(cat /proc/sys/kernel/random/uuid)
+tquic="$(cat ~/log-install.txt | grep -w "TROJAN QUIC" | cut -d: -f2|sed 's/ //g')"
 tgrpc="$(cat ~/log-install.txt | grep -w "TROJAN GRPC" | cut -d: -f2|sed 's/ //g')"
 txtls="$(cat ~/log-install.txt | grep -w "TROJAN XTLS" | cut -d: -f2|sed 's/ //g')"
 tgfw="$(cat ~/log-install.txt | grep -w "TROJAN GFW" | cut -d: -f2|sed 's/ //g')"
@@ -84,6 +85,8 @@ sed -i '/#trojan-gfw$/a\#&# '"$user $exp"'\
 },{"password": "'""$uuid""'","email": "'""$user""'"' /usr/local/etc/xray/xvmess.json
 sed -i '/#trojan-nontls$/a\#&# '"$user $exp"'\
 },{"password": "'""$uuid""'","email": "'""$user""'"' /etc/xray/xtrojan.json
+sed -i '/#trojan-quic$/a\#&# '"$user $exp"'\
+},{"password": "'""$uuid""'","email": "'""$user""'"' /etc/xray/xtrojan.json
 sed -i '/#trojan-http-tls$/a\#&# '"$user $exp"'\
 },{"password": "'""$uuid""'","email": "'""$user""'"' /usr/local/etc/xray/xvmess.json
 sed -i '/#trojan-http-tls$/a\#&# '"$user $exp"'\
@@ -103,15 +106,17 @@ hariini=`date -d "0 days" +"%Y-%m-%d"`
 echo -e "### $user $exp" >> /etc/trojan-go/akun.conf
 systemctl restart trojan-go.service
 trojango="trojan-go://${uuid}@${domain}:${trgo}/?sni=${domain}&type=ws&host=${domain}&path=/gandring&encryption=none#%F0%9F%94%B0TROJAN+GO+$user"
-trojanhdua="trojan://${uuid}@${domain}:$thdua?sni=jambualas.net&type=http&security=tls&path=/gandringhttp#%F0%9F%94%B0TROJAN+H2C+${user}"
-trojangrpc="trojan://${uuid}@${domain}:$tgrpc?mode=gun&security=tls&type=grpc&serviceName=/gandringgrpc&sni=${domain}#%F0%9F%94%B0TROJAN+GRPC+${user}"
+trojanhdua="trojan://${uuid}@${domain}:$thdua?sni=jambualas.net&type=http&security=tls&path=/gandringhttp#%F0%9F%94%B0TROJAN+H2C+TLS+${user}"
+trojangrpc="trojan://${uuid}@${domain}:$tgrpc?mode=gun&security=tls&type=grpc&serviceName=/gandringgrpc&sni=${domain}#%F0%9F%94%B0TROJAN+GRPC+TLS+${user}"
 trojanxtls="trojan://${uuid}@${domain}:$txtls?sni=kimcil.kepolen.com&security=xtls&type=tcp&headerType=none&flow=xtls-rprx-splice-udp443#%F0%9F%94%B0TROJAN+XTLS+${user}"
-trojangfw="trojan://$uuid@$domain:$tgfw?sni=pemetikbunga.com&type=tcp&security=tls&headerType=none#%F0%9F%94%B0TROJAN+GFW+$user"
+trojangfw="trojan://$uuid@$domain:$tgfw?sni=pemetikbunga.com&type=tcp&security=tls&headerType=none#%F0%9F%94%B0TROJAN+GFW+TLS+$user"
 trojantls="trojan://${uuid}@${domain}:$ttls?type=ws&security=tls&host=$domain&path=%2fgandring&sni=$domain#%F0%9F%94%B0TROJAN+WS+TLS+${user}"
 trojannontls="trojan://${uuid}@${domain}:$tnontls?type=ws&security=none&host=$domain&path=%2fgandring#%F0%9F%94%B0TROJAN+WS+NONTLS+${user}"
 trojanhttp="trojan://${uuid}@${domain}:$thttp?sni=gesekan.penghancur-janda.com&type=tcp&security=tls&host=$domain&path=/gandringtcp&headerType=http#%F0%9F%94%B0+HTTP+TLS+${user}"
 trojanhttpnon="trojan://${uuid}@${domain}:$thttpnon?sni=gesekan.penghancur-janda.com&type=tcp&security=none&host=$domain&headerType=http#%F0%9F%94%B0TROJAN+HTTP+NONTLS+${user}"
+trojanquic="trojan://$uuid@$domain:$tquic?sni=$domain&quicSecurity=$domain&key=%2Fgandringquic&security=tls&type=quic&headerType=none#%F0%9F%94%B0TROJAN+QUIC+TLS+$user"
 systemctl restart xray.service
+
 systemctl restart xtrojan.service
 systemctl restart xvmess
 systemctl restart satrio
@@ -129,31 +134,35 @@ echo -e "ServiceName      :/gandringrpc"
 echo -e "Path HTTP        :/gandringtcp"
 echo -e "Path H2C         :/gandringhttp"
 echo -e "Path WS          :/gandring"
+echo -e "Path QUIC        :/gandringquic"
 echo -e "Port GRPC        :${tgrpc}"
 echo -e "Port WSTLS       :${ttls} , $tnontls"
 echo -e "Port H2C         :${thdua}"
 echo -e "Port HTTP TLS    :${thttp} , $thttpnon"
 echo -e "Port GFW         :${tgfw}"
-echo -e "Port XTLS        :${txtls}" 
+echo -e "Port XTLS        :${txtls}"
+echo -e "Port QUIC        :${tquic}"
 echo -e "Sandi            :${uuid}"
 echo -e "Dibuat           :$hariini"
 echo -e "Kadaluarsa       :$exp"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Link GRPC:  ${trojangrpc}"
+echo -e "Link TROJAN GRPC:  ${trojangrpc}"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Link XTLS:  ${trojanxtls}"
+echo -e "Link TROJAN XTLS:  ${trojanxtls}"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Link GFW:  ${trojangfw}"
+echo -e "Link TROJAN GFW:  ${trojangfw}"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Link HTTP/2:  ${trojanhdua}"
+echo -e "Link TROJAN HTTP/2:  ${trojanhdua}"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Link WS TLS:  ${trojantls}"
+echo -e "Link TROJAN WS TLS:  ${trojantls}"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Link WS NONTLS:  ${trojannontls}"
+echo -e "Link TROJAN WS NONTLS:  ${trojannontls}"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Link HTTP TLS:  ${trojanhttp}"
+echo -e "Link TROJAN HTTP TLS:  ${trojanhttp}"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Link HTTP NONTLS:  ${trojanhttpnon}"
+echo -e "Link TROJAN HTTP NONTLS:  ${trojanhttpnon}"
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "Link TROJAN QUIC:  ${trojanquic}"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "Link TROJAN GO:  ${trojango}"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
