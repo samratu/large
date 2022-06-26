2c9ea9b6d6fb90b7204ff444a0e5e9a206d596bd
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-06-27 05:53:31 +0700

    Update renewtrojanhdua.sh

--- xray/renewtrojanhdua.sh
+++ xray/renewtrojanhdua.sh
@@ -98,6 +98,48 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#&# " "/etc/xray/config.json")
 			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
 		fi
 	done
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/trojangrpc.json")
+	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+		echo ""
+		echo "You have no existing clients!"
+		exit 1
+	fi
+
+	clear
+	echo ""
+	echo " Select the existing client you want to remove"
+	echo " Press CTRL+C to return"
+	echo " ==============================="
+	echo "     No  Expired   User"
+	grep -E "^### " "/etc/xray/trojangrpc.json" | cut -d ' ' -f 2-3 | nl -s ') '
+	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
+		if [[ ${CLIENT_NUMBER} == '1' ]]; then
+			read -rp "Select one client [1]: " CLIENT_NUMBER
+		else
+			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
+		fi
+	done
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/vlessquic.json")
+	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+		echo ""
+		echo "You have no existing clients!"
+		exit 1
+	fi
+
+	clear
+	echo ""
+	echo " Select the existing client you want to remove"
+	echo " Press CTRL+C to return"
+	echo " ==============================="
+	echo "     No  Expired   User"
+	grep -E "^### " "/etc/xray/vlessquic.json" | cut -d ' ' -f 2-3 | nl -s ') '
+	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
+		if [[ ${CLIENT_NUMBER} == '1' ]]; then
+			read -rp "Select one client [1]: " CLIENT_NUMBER
+		else
+			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
+		fi
+	done
 read -p "Expired (Days) : " masaaktif
 user=$(grep -E "^#&# " "/etc/xray/xtrojan.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
 exp=$(grep -E "^#&# " "/etc/xray/xtrojan.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
@@ -107,6 +149,12 @@ user=$(grep -E "^#&# " "/etc/xray/xvless.json" | cut -d ' ' -f 2 | sed -n "${CLI
 exp=$(grep -E "^#&# " "/etc/xray/xvless.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
 user=$(grep -E "^#&# " "/etc/xray/config.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
 exp=$(grep -E "^#&# " "/etc/xray/config.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+user=$(grep -E "^### " "/etc/xray/trojangrpcjson" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^### " "/etc/xray/trojangrpc.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+sed -i "/^### $user $exp/,/^},{/d" /etc/xray/trojangrpc.json
+user=$(grep -E "^### " "/etc/xray/vlessquicjson" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^### " "/etc/xray/vlessquic.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+sed -i "/^### $user $exp/,/^},{/d" /etc/xray/vlessquic.json
 now=$(date +%Y-%m-%d)
 d1=$(date -d "$exp" +%s)
 d2=$(date -d "$now" +%s)
@@ -117,6 +165,8 @@ sed -i "s/#&# $user $exp/#&# $user $exp4/g" /etc/xray/xtrojan.json
 sed -i "s/#&# $user $exp/#&# $user $exp4/g" /usr/local/etc/xray/xvmess.json
 sed -i "s/#&# $user $exp/#&# $user $exp4/g" /etc/xray/xvless.json
 sed -i "s/#&# $user $exp/#&# $user $exp4/g" /etc/xray/config.json
+sed -i "s/#&# $user $exp/#&# $user $exp4/g" /etc/xray/trojangrpc.json
+sed -i "s/#&# $user $exp/#&# $user $exp4/g" /etc/xray/vlessquic.json
 clear
 echo ""
 echo "================================"
