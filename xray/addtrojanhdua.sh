5d3b03ba22d4f5c2e702d7a322346e705b8c28b2
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-06-26 10:00:18 +0700

    Update addtrojanhdua.sh

--- xray/addtrojanhdua.sh
+++ xray/addtrojanhdua.sh
@@ -48,7 +48,7 @@ sed -i '/#trojan-hdua$/a\#&# '"$user $exp"'\
 },{"password": "'""$uuid""'","email": "'""$user""'"' /etc/xray/xtrojan.json
 sed -i '/#trojan-hdua$/a\#&# '"$user $exp"'\
 },{"password": "'""$uuid""'","email": "'""$user""'"' /usr/local/etc/xray/xvmess.json
-trojanhdua="trojan://${uuid}@${domain}:$thdua?sni=${domain}&type=http&security=tls&path=gandringhttp#%F0%9F%94%B0TROJAN+H2C+TLS+${user}"
+trojanhdua="trojan://${uuid}@${domain}:$thdua?sni=${domain}&type=http&security=tls&path=/gandringhttp#%F0%9F%94%B0TROJAN+H2C+TLS+${user}"
 systemctl restart xray.service
 systemctl restart xtrojan.service
 systemctl restart xvmess.service
@@ -62,7 +62,7 @@ echo -e "Nama :${user}"
 echo -e "IP/Host :${MYIP}"
 echo -e "Address :${domain}"
 echo -e "Protocol :H2C"
-echo -e "Path :gandringhttp"
+echo -e "Path :/gandringhttp"
 echo -e "Port :${thdua}"
 echo -e "Password :${uuid}"
 echo -e "Dibuat :$hariini"
