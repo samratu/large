cb82cb2c909c2c0dc33cd06052f96c2a34f1e57f
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-06-26 15:08:28 +0700

    Update cekvlessquic.sh

--- xray/cekvlessquic.sh
+++ xray/cekvlessquic.sh
@@ -16,6 +16,7 @@ MYIP=$(wget -qO- ipinfo.io/ip);
 clear
 echo -n > /tmp/other.txt
 data=( `cat /etc/xray/xvless.json | grep '^####' | cut -d ' ' -f 2`);
+data=( `cat /etc/xray/vlessquic.json | grep '^####' | cut -d ' ' -f 2`);
 data=( `cat /usr/local/etc/xray/xvmess.json | grep '^#&#' | cut -d ' ' -f 2`);
 data=( `cat /etc/xray/config.json | grep '^#&#' | cut -d ' ' -f 2`);
 data=( `cat /usr/local/etc/xray/satrio.json | grep '^#&#' | cut -d ' ' -f 2`);
