5731b522e2f0d0788726098ddb19e0e56b16c46e
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-06-29 02:19:13 +0700

    Update cekvlessquic.sh

--- xray/cekvlessquic.sh
+++ xray/cekvlessquic.sh
@@ -20,6 +20,7 @@ data=( `cat /etc/xray/trojangrpc.json | grep '^#&#' | cut -d ' ' -f 2`);
 data=( `cat /usr/local/etc/xray/xvmess.json | grep '^#&#' | cut -d ' ' -f 2`);
 data=( `cat /etc/xray/xvless.json | grep '^#&#' | cut -d ' ' -f 2`);
 data=( `cat /etc/xray/config.json | grep '^#&#' | cut -d ' ' -f 2`);
+data=( `cat /usr/local/etc/xray/vlessquic.json | grep '^#&#' | cut -d ' ' -f 2`);
 echo "----------------------------------------";
 echo "---------=[ Vless User Login ]=---------";
 echo "----------------------------------------";
