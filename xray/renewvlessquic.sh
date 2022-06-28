7aa992c1689fcaa8e5bc02f5190b4de880aeda14
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-06-29 02:24:32 +0700

    Update renewvlessquic.sh

--- xray/renewvlessquic.sh
+++ xray/renewvlessquic.sh
@@ -36,7 +36,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#&# " "/etc/xray/xtrojan.json")
 			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
 		fi
 	done
-NUMBER_OF_CLIENTS=$(grep -c -E "^#&# " "/etc/xray/vlessquic.json")
+NUMBER_OF_CLIENTS=$(grep -c -E "^#&# " "/usr/local/etc/xray/vlessquic.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		clear
 		echo ""
@@ -49,7 +49,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#&# " "/etc/xray/vlessquic.json")
 	echo "Select the existing client you want to renew"
 	echo " Press CTRL+C to return"
 	echo -e "==============================="
-	grep -E "^#&# " "/etc/xray/vlessquic.json" | cut -d ' ' -f 2-3 | nl -s ') '
+	grep -E "^#&# " "/usr/local/etc/xray/vlessquic.json" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
 			read -rp "Select one client [1]: " CLIENT_NUMBER
@@ -130,7 +130,7 @@ exp=$(grep -E "^#&# " "/etc/xray/xvless.json" | cut -d ' ' -f 3 | sed -n "${CLIE
 user=$(grep -E "^#&# " "/etc/xray/config.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
 exp=$(grep -E "^#&# " "/etc/xray/config.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
 user=$(grep -E "^#&# " "/etc/xray/vlessquic.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
-exp=$(grep -E "^#&# " "/etc/xray/vlessquic.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^#&# " "/usr/local/etc/xray/vlessquic.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
 now=$(date +%Y-%m-%d)
 d1=$(date -d "$exp" +%s)
 d2=$(date -d "$now" +%s)
@@ -141,7 +141,7 @@ sed -i "s/#&# $user $exp/#&# $user $exp4/g" /etc/xray/xtrojan.json
 sed -i "s/#&# $user $exp/#&# $user $exp4/g" /usr/local/etc/xray/xvmess.json
 sed -i "s/#&# $user $exp/#&# $user $exp4/g" /etc/xray/xvless.json
 sed -i "s/#&# $user $exp/#&# $user $exp4/g" /etc/xray/config.json
-sed -i "s/#&# $user $exp/#&# $user $exp4/g" /etc/xray/vlessquic.json
+sed -i "s/#&# $user $exp/#&# $user $exp4/g" /usr/local/etc/xray/vlessquic.json
 clear
 echo ""
 echo "================================"
