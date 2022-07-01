076dd99c7cfd520b6c489b168c051a04f3016449
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-07-01 11:27:59 +0700

    Update certv2ray.sh

--- xray/certv2ray.sh
+++ xray/certv2ray.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+1#!/bin/bash
 echo start
 sleep 0.5
 source /var/lib/wisnucs/ipvps.conf
@@ -14,24 +14,22 @@ curl https://get.acme.sh | sh
 alias acme.sh=~/.acme.sh/acme.sh
 /root/.acme.sh/acme.sh --upgrade --auto-upgrade
 /root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+#/root/.acme.sh/acme.sh --issue -d "${domain}" --standalone --keylength ec-2048
 /root/.acme.sh/acme.sh --issue -d "${domain}" --standalone --keylength ec-256
 /root/.acme.sh/acme.sh --install-cert -d "${domain}" --ecc \
---fullchain-file /etc/xray/xray.cer \
-#--key-file /etc/xray/xray.key
-#chown -R nobody:nogroup /etc/xray
-#chmod 644 /etc/xray/xray.cer
-#chmod 644 /etc/xray/xray.key
 --fullchain-file /etc/ssl/private/fullchain.pem \
 --key-file /etc/ssl/private/privkey.pem
 chown -R nobody:nogroup /etc/xray
-chmod 644 /etc/ssl/private/fullchain.pem
 chmod 644 /etc/ssl/private/privkey.pem
+chmod 644 /etc/ssl/private/fullchain.pem
 systemctl start xray
 systemctl start xtrojan
 systemctl start xvless
 systemctl restart xvmess
 systemctl restart xss
-systemctl restart satrio
+systemctl restart vlessquic
+systemctl restart sstcp
+systemctl restart trojangrpc
 echo Done
 sleep 0.5
 clear 
