777e2a035899e0f31c38d1628da52d3f675d37c0
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-07-01 20:13:49 +0700

    Update xp.sh

--- ssh/xp.sh
+++ ssh/xp.sh
@@ -249,6 +249,26 @@ systemctl start xvmess.service
 systemctl enable xvmess.service
 systemctl restart xvmess.service
 
+exp=$(grep -w "^#### $user" "/usr/local/etc/xray/vlessquic.json" | cut -d ' ' -f 3)
+#exp=$(grep -w "^#### $user" "/etc/v2ray/config.json" | cut -d ' ' -f 3)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" = "0" ]]; then
+sed -i "/^#### $user $exp/,/^},{/d" /usr/local/etc/xray/vlessquic.json
+sed -i "/^#### $user $exp/,/^},{/d" /usr/local/etc/xray/xvmess.json
+#sed -i "/^#### $user $exp/,/^},{/d" /etc/v2ray/config.json
+#sed -i "/^#### $user $exp/,/^},{/d" /etc/v2ray/config.json
+fi
+done
+##restart&start service
+systemctl daemon-reload
+systemctl stop xvmess.service
+systemctl start xvmess.service
+systemctl enable xvmess.service
+systemctl restart xvmess.service
+
+
 exp=$(grep -w "^#### $user" "/etc/xray/xss.json" | cut -d ' ' -f 3)
 #exp=$(grep -w "^#### $user" "/etc/v2ray/config.json" | cut -d ' ' -f 3)
 d1=$(date -d "$exp" +%s)
