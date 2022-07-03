5205d2a70aef17f7aa9e0d23e71692b2ab351e02
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-07-04 05:03:02 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -197,7 +197,7 @@ apt-get --reinstall --fix-missing install -y bzip2 gzip coreutils wget screen rs
 echo "status" >> .profile
 sleep 1p
 # install webserver
-mkdir -p /etc/nginx/conf.d/
+mkdir -p /etc/nginx/conf.d
 apt -y install nginx php php-fpm php-cli php-mysql libxml-parser-perl
 sudo pkill -f nginx & wait $!
 systemctl stop nginx
@@ -433,7 +433,7 @@ cat > /etc/systemd/system/stunnel5.service << END
 Description=Stunnel5 Service
 Documentation=https://stunnel.org
 Documentation=https://t.me/zerossl
-After=network.target
+After=syslog.target network-online.target
 
 [Service]
 Type=forking
