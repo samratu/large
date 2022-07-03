827389d0ef2351e42613f41d15aff9ded1687064
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-07-03 23:03:57 +0700

    Update cf.sh

--- ssh/cf.sh
+++ ssh/cf.sh
@@ -15,6 +15,20 @@ LIGHT='\033[0;37m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 clear
 apt install jq curl -y
+rm -f /root/domain
+rm -f /etc/v2ray/domain
+rm -f /etc/xray/domain
+rm -rf /etc/xray/domain
+rm -rf /root/nsdomain
+rm -rf /var/lib/wisnucs/ipvps.conf
+rm nsdomain
+rm domain
+mkdir -p /usr/bin/xray
+mkdir -p /usr/bin/v2ray
+mkdir -p /etc/xray
+mkdir -p /etc/v2ray
+echo "$SUB_DOMAIN" >> /etc/v2ray/domain
+echo "$SUB_DOMAIN" >> /etc/xray/domain
 DOMAIN=zerossl.my.id
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c2)
 SUB_DOMAIN=${sub}.zerossl.my.id
@@ -74,6 +88,31 @@ RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_r
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" \
      --data '{"type":"A","name":"'${WILD_DOMAIN}'","content":"'${IP}'","ttl":300,"proxied":false}')
+fi
+echo "Updating DNS NS for ${NS_DOMAIN}..."
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${NS_DOMAIN}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+if [[ "${#RECORD}" -le 10 ]]; then
+     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"NS","name":"'${NS_DOMAIN}'","content":"'${SUB_DOMAIN}'","ttl":120,"proxied":true}' | jq -r .result.id)
+fi
+
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"NS","name":"'${NS_DOMAIN}'","content":"'${SUB_DOMAIN}'","ttl":120,"proxied":true}')
 echo "Host : $SUB_DOMAIN"
 echo $SUB_DOMAIN > /root/domain
 # / / Make Main Directory
@@ -83,3 +122,14 @@ mkdir -p /etc/xray
 cp /root/domain /etc/xray
 cp /root/domain /usr/local/etc/xray
 rm -f /root/cf.sh
+rm -rf /etc/xray/domain
+rm -rf /root/nsdomain
+echo "IP=""$SUB_DOMAIN" >> /var/lib/wisnucs/ipvps.conf
+echo "Host : $SUB_DOMAIN"
+echo $SUB_DOMAIN > /root/domain
+echo "Host SlowDNS : $NS_DOMAIN"
+echo "$NS_DOMAIN" >> /root/nsdomain
+echo "$SUB_DOMAIN" >> /etc/xray/domain
+echo "$SUB_DOMAIN" >> /usr/local/etc/xray/domain
+echo "$SUB_DOMAIN" >> /ssl/xray
+cd
