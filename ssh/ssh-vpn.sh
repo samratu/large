8baf795e11b206a82481c59310a07860e869c132
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-07-06 01:03:06 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -194,7 +194,7 @@ mkdir /ssl/xray
 
 # install
 
-#echo "neofetch" >> .profile
+echo "neofetch" >> .profile
 echo "status" >> .profile
 sleep 1p
 # install webserver
@@ -351,7 +351,7 @@ RUN=yes
 # systemd users: don't forget to modify /lib/systemd/system/sslh.service
 DAEMON=/usr/sbin/sslh
 
-DAEMON_OPTS="--user sslh --listen 0.0.0.0:443 --ssl 127.0.0.1:500 --ssh 127.0.0.1:300 --openvpn 127.0.0.1:1194 --http 127.0.0.1:80 --pidfile /var/run/sslh/sslh.pid -n"
+DAEMON_OPTS="--user sslh --listen 0.0.0.0:443 --ssl 127.0.0.1:500 --ssh 127.0.0.1:42 --dropbear 127.0.0.1:300 --openvpn 127.0.0.1:1194 --http 127.0.0.1:80 --pidfile /var/run/sslh/sslh.pid -n"
 
 END
 
@@ -417,11 +417,11 @@ socket = r:TCP_NODELAY=1
 
 [dropbear]
 accept = 600
-connect = 127.0.0.1:200
+connect = 127.0.0.1:300
 
 [openssh]
 accept = 500
-connect = 127.0.0.1:443
+connect = 127.0.0.1:42
 
 [openvpn]
 accept = 990
