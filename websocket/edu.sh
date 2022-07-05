aa4deda2c5ff5436dde099c5bf58e1c91a1b8793
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-07-05 09:05:36 +0700

    Add files via upload

--- websocket/edu.sh
+++ websocket/edu.sh
@@ -1,32 +1,21 @@
 #!/bin/bash
 # Proxy For Edukasi & Imclass
-# My Telegram : https://t.me/zerossl
+# SL
 # ==========================================
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# ==========================================
-# Getting
-MYIP=$(wget -qO- ipinfo.io/ip);
+
 # Link Hosting Kalian
-wisnuvpn="raw.githubusercontent.com/samratu/large/file/websocket"
-#wget -q -O /usr/bin/proxy3.js "https://raw.githubusercontent.com/samratu/large/file/websocket/proxy3.js"
+akbarvpn="raw.githubusercontent.com/fisabiliyusri/Mantap/main/websocket"
+
 # Getting Proxy Template
-wget -q -O /usr/local/bin/ws-nontls https://${wisnuvpn}/ws-nontls.py
+wget -q -O /usr/local/bin/ws-nontls https://${akbarvpn}/websocket.py
 chmod +x /usr/local/bin/ws-nontls
+
 # Installing Service
-cd
 cat > /etc/systemd/system/ws-nontls.service << END
 [Unit]
-Description=SSHWS BENDUNG COLO PENGKOL BY GANDRING
-Documentation=https://t.me/zerossl
-After=network.target
+Description=Python Proxy Mod By Akbar Maulana
+Documentation=https://nekopi.care
+After=network.target nss-lookup.target
 
 [Service]
 Type=simple
@@ -34,9 +23,8 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/ws-nontls 2086
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-nontls 8880
 Restart=on-failure
-RestartSec=1s
 
 [Install]
 WantedBy=multi-user.target
@@ -47,15 +35,15 @@ systemctl enable ws-nontls
 systemctl restart ws-nontls
 
 # Getting Proxy Template
-wget -q -O /usr/local/bin/ovpnws https://${wisnuvpn}/ovpnws.py
-chmod +x /usr/local/bin/ovpnws
+wget -q -O /usr/local/bin/ws-ovpn https://${akbarvpn}/ws-ovpn.py
+chmod +x /usr/local/bin/ws-ovpn
 
 # Installing Service
-cat > /etc/systemd/system/ovpnws.service << END
+cat > /etc/systemd/system/ws-ovpn.service << END
 [Unit]
-Description=OVPNWS BENDUNG COLO PENGKOL BY GANDRING
-Documentation=https://t.me/zerossl
-After=network.target
+Description=Python Proxy Mod By LamVpn
+Documentation=https://nekopoi.care
+After=network.target nss-lookup.target
 
 [Service]
 Type=simple
@@ -63,28 +51,27 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/ovpnws 8080
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-ovpn 2086
 Restart=on-failure
-RestartSec=1s
 
 [Install]
 WantedBy=multi-user.target
 END
 
 systemctl daemon-reload
-systemctl enable ovpnws
-systemctl restart ovpnws
+systemctl enable ws-ovpn
+systemctl restart ws-ovpn
 
 # Getting Proxy Template
-wget -q -O /usr/local/bin/ws-tls https://${wisnuvpn}/ws-tls.py
+wget -q -O /usr/local/bin/ws-tls https://${akbarvpn}/ws-tls
 chmod +x /usr/local/bin/ws-tls
 
 # Installing Service
 cat > /etc/systemd/system/ws-tls.service << END
 [Unit]
-Description=SSHWS GAJAH DEMAK BY WISNUCOKROSATRIO
-Documentation=https://t.me/zerossl
-After=network.target
+Description=Python Proxy Mod By geovpn
+Documentation=https://nekopoi.care
+After=network.target nss-lookup.target
 
 [Service]
 Type=simple
@@ -92,9 +79,8 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/ws-tls 2087
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-tls 443
 Restart=on-failure
-RestartSec=1s
 
 [Install]
 WantedBy=multi-user.target
@@ -103,60 +89,3 @@ END
 systemctl daemon-reload
 systemctl enable ws-tls
 systemctl restart ws-tls
-
-# Getting Proxy Template
-wget -q -O /usr/local/bin/wstunnel https://${wisnuvpn}/wstunnel.py
-chmod +x /usr/local/bin/wstunnel
-
-# Installing Service
-cat > /etc/systemd/system/wstunnel.service << END
-[Unit]
-Description=WSTUNNEL BENDUNG COLO PENGKOL BY GANDRING
-Documentation=https://t.me/zerossl
-After=network.target
-
-[Service]
-Type=simple
-User=root
-CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/wstunnel
-Restart=on-failure
-RestartSec=1s
-
-[Install]
-WantedBy=multi-user.target
-END
-
-systemctl daemon-reload
-systemctl enable wstunnel
-systemctl restart wstunnel
-
-wget -q -O /usr/local/bin/sshws https://${wisnuvpn}/websocket.py
-chmod +x /usr/local/bin/sshws
-
-# Installing Service
-cat > /etc/systemd/system/sshws.service << END
-[Unit]
-Description=SSHWEBSOCKET BENDUNG COLO PENGKOL BY GANDRING
-Documentation=https://t.me/zerossl
-After=network.target
-
-[Service]
-Type=simple
-User=root
-CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/sshws 300
-Restart=on-failure
-RestartSec=1s
-
-[Install]
-WantedBy=multi-user.target
-END
-
-systemctl daemon-reload
-systemctl enable sshws
-systemctl restart sshws
