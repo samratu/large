06f19eea551f3b392181b950f83473bfaf5950f2
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-07-06 14:43:24 +0700

    Add files via upload

--- websocket/edu.sh
+++ websocket/edu.sh
@@ -4,17 +4,17 @@
 # ==========================================
 
 # Link Hosting Kalian
-wisnuvpn="raw.githubusercontent.com/samratu/large/file/websocket"
+akbarvpn="raw.githubusercontent.com/fisabiliyusri/Mantap/main/websocket"
 
 # Getting Proxy Template
-wget -q -O /usr/local/bin/ws-nontls https://${wisnuvpn}/websocket.py
+wget -q -O /usr/local/bin/ws-nontls https://${akbarvpn}/websocket.py
 chmod +x /usr/local/bin/ws-nontls
 
 # Installing Service
 cat > /etc/systemd/system/ws-nontls.service << END
 [Unit]
-Description=SSHWEBSOCKET ROUTING DAM COLO BY GANDRING
-Documentation=https://t.me/zerossl
+Description=Python Proxy Mod By Akbar Maulana
+Documentation=https://nekopi.care
 After=network.target nss-lookup.target
 
 [Service]
@@ -23,12 +23,11 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/ws-nontls 2086
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-nontls 8880
 Restart=on-failure
 
 [Install]
 WantedBy=multi-user.target
-
 END
 
 systemctl daemon-reload
@@ -36,14 +35,14 @@ systemctl enable ws-nontls
 systemctl restart ws-nontls
 
 # Getting Proxy Template
-wget -q -O /usr/local/bin/ws-ovpn https://${wisnuvpn}/ws-ovpn.py
+wget -q -O /usr/local/bin/ws-ovpn https://${akbarvpn}/ws-ovpn.py
 chmod +x /usr/local/bin/ws-ovpn
 
 # Installing Service
 cat > /etc/systemd/system/ws-ovpn.service << END
 [Unit]
-Description=OVPN WEBSOCKET ROUTING GAJAH BY WISNU
-Documentation=https://t.mezerossl
+Description=Python Proxy Mod By LamVpn
+Documentation=https://nekopoi.care
 After=network.target nss-lookup.target
 
 [Service]
@@ -52,12 +51,11 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/ws-ovpn 8080
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-ovpn 2086
 Restart=on-failure
 
 [Install]
 WantedBy=multi-user.target
-
 END
 
 systemctl daemon-reload
@@ -65,14 +63,14 @@ systemctl enable ws-ovpn
 systemctl restart ws-ovpn
 
 # Getting Proxy Template
-wget -q -O /usr/local/bin/ws-tls https://${wisnuvpn}/ws-tls
+wget -q -O /usr/local/bin/ws-tls https://${akbarvpn}/ws-tls
 chmod +x /usr/local/bin/ws-tls
 
 # Installing Service
 cat > /etc/systemd/system/ws-tls.service << END
 [Unit]
-Description=SSHWEBSOCKET TLS ROUTING PENGKOL BY SHANUM
-Documentation=https://t.me/zerossl
+Description=Python Proxy Mod By geovpn
+Documentation=https://nekopoi.care
 After=network.target nss-lookup.target
 
 [Service]
@@ -81,12 +79,11 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/ws-tls 2087
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-tls 443
 Restart=on-failure
 
 [Install]
 WantedBy=multi-user.target
-
 END
 
 systemctl daemon-reload
--- websocket/ws-ovpn.py
+++ websocket/ws-ovpn.py
@@ -4,7 +4,7 @@ import socket, threading, thread, select, signal, sys, time, getopt
 # Listen
 LISTENING_ADDR = '0.0.0.0'
 LISTENING_PORT = sys.argv[1]
-LISTENING_PORT = 8080
+
 # Pass
 PASS = ''
 
@@ -12,7 +12,7 @@ PASS = ''
 BUFLEN = 4096 * 4
 TIMEOUT = 60
 DEFAULT_HOST = '127.0.0.1:1194'
-RESPONSE = 'HTTP/1.1 520 <b><font color="green">SUN AMATAK AJIKU SI JARAN GOYANG TAK GOYANG ING TENGAH LATAR CEMETHIKU SODHO LANANG SAKING SWARGO TAK SABETAKE GUNUNG JUGRUG TAK SABETAKE SEGORO ASAT TAK SABETAKE MARANG ATINE SI JABANG BAYI PET SIDHO EDAN ORA EDAN SIDHO GENDENG ORA GENDENG ORA MARI MARI YEN ORA INGSUN SING NAMBANI </font></b>\r\n\r\nContent-Length: 104857600000\r\n\r\n'
+RESPONSE = 'HTTP/1.1 101 Switching Protocols\r\n\r\nContent-Length: 104857600000\r\n\r\n'
 
 class Server(threading.Thread):
     def __init__(self, host, port):
