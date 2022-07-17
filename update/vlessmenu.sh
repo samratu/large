d6204627feac5f7a1261fdc2e903536fb128a2a6
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-07-17 12:30:27 +0700

    Update vlessmenu.sh

--- update/vlessmenu.sh
+++ update/vlessmenu.sh
@@ -1,45 +1,45 @@
 #!/bin/bash
-# gandring
-
-#cat /usr/bin/bannerVLESS | lolcat
+# wisnu
+clear
+#cat /usr/bin/bannerTROJAN | lolcat
 echo -e ""
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[43;1;46m               🔰 MENU LAYANAN VLESS 🔰                 \e[0m"
+echo -e "\E[43;1;46m   🔰 MENU VLESS XTLS GFW H2C GRPC WS HTTP & GO 🔰     \e[0m"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "01)  Buat Akun Vless Websocket"
-echo -e "02)  Hapus Akun Vless Websocket"
-echo -e "03)  Perpanjang Masa Aktif Akun Vless Websocket"
-echo -e "04)  Cek User Login Vless Websocket"
+echo -e "01\e[1;33m) Buat Akun VLESS WEBSOCKET  \e[m"
+echo -e "02\e[1;33m) Hapus Akun VLESS WEBSOCKET  \e[m"
+echo -e "03\e[1;33m) Perpanjang Masa Aktif Akun VLESS WEBSOCKET  \e[m"
+echo -e "04\e[1;33m) Cek User Login VLESS WEBSOCKET  \e[m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "05)  Buat Akun Vless gRPC"
-echo -e "06)  Hapus Akun Vless gRPC"
-echo -e "07)  Perpanjang Masa Aktif Akun Vless gRPC"
-echo -e "08)  Cek User Login Vless gRPC"
+echo -e "05\e[1;33m) Buat Akun VLESS GRPC \e[m"
+echo -e "06\e[1;33m) Hapus Akun VLESS GRPC \e[m"
+echo -e "07\e[1;33m) Perpanjang Masa Aktif Akun VLESS GRPC \e[m"
+echo -e "08\e[1;33m) Cek User Login VLESS GRPC \e[m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "09)  Buat Akun Vless XTLS"
-echo -e "10)  Hapus Akun Vless XTLS"
-echo -e "11)  Perpanjang Masa Aktif Akun Vless XTLS"
-echo -e "12)  Cek User Login Vless XTLS"
+echo -e "09\e[1;33m) Buat Akun VLESS H2C \e[m"
+echo -e "10\e[1;33m) Hapus Akun VLESS H2C \e[m"
+echo -e "11\e[1;33m) Perpanjang Masa Aktif Akun VLESS H2C \e[m"
+echo -e "12\e[1;33m) Cek User Login VLESS H2C \e[m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "13)  Buat Akun VLESS H2C"
-echo -e "14)  Hapus Akun VLESS H2C"
-echo -e "15)  Perpanjang Masa Aktif Akun VLESS H2C"
-echo -e "16)  Cek User Login VLESS H2C"
+echo -e "13\e[1;33m) Buat Akun VLESS XTLS \e[m"
+echo -e "14\e[1;33m) Hapus Akun VLESS XTLS  \e[m"
+echo -e "15\e[1;33m) Perpanjang Masa Aktif Akun VLESS XTLS  \e[m"
+echo -e "16\e[1;33m) Cek User Login VLESS XTLS  \e[m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "17)  Buat Akun VLESS HTTP"
-echo -e "18)  Hapus Akun VLESS HTTP"
-echo -e "19)  Perpanjang Masa Aktif Akun VLESS HTTP"
-echo -e "20)  Cek User Login VLESS HTTP"
+echo -e "17\e[1;33m) Buat Akun VLESS HTTP \e[m"
+echo -e "18\e[1;33m) Hapus Akun VLESS HTTP \e[m"
+echo -e "19\e[1;33m) Perpanjang Masa Aktif Akun VLESS HTTP \e[m"
+echo -e "20\e[1;33m) Cek User Login VLESS HTTP \e[m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "21)  Buat Akun VLESS QUIC"
-echo -e "22)  Hapus Akun VLESS QUIC"
-echo -e "23)  Perpanjang Masa Aktif Akun VLESS QUIC"
-echo -e "24)  Cek User Login VLESS QUIC"
+echo -e "21\e[1;33m) Buat Akun VLESS QUIC \e[m"
+echo -e "22\e[1;33m) Hapus Akun VLESS QUIC \e[m"
+echo -e "23\e[1;33m) Perpanjang Masa Aktif Akun VLESS QUIC \e[m"
+echo -e "24\e[1;33m) Cek User Login VLESS QUIC \e[m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "00\e[1;31m)  Menu Utama \e[m"
-echo -e "xx\e[1;31m)  Keluar \e[m"
+echo -e "00\e[1;31m) Menu Utama \e[m"
+echo -e "xx\e[1;31m) Keluar \e[m"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[43;1;46m            🔰 LUXURY EDITION BY ZEROSSL 🔰             \e[0m"
+echo -e "\E[43;1;46m             🔰 LUXURY EDITION BY ZEROSSL 🔰            \e[0m"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 read -p "Silahkan Pilih Angka[ 1 - 0 ] : " menu
 echo -e ""
@@ -63,34 +63,34 @@ addvlessgrpc
 delvlessgrpc
 ;;
 7)
-renewvlessgrpc
+renewvlessgroc
 ;;
 8)
 cekvlessgrpc
 ;;
 9)
-addvlessxtls
-;;
-10)
-delvlessxtls
-;;
-11)
-renewvlessxtls
-;;
-12)
-cekvlessxtls
-;;
-13)
 addvlesshdua
 ;;
-14)
+10)
 delvlesshdua
 ;;
-15)
+11)
 renewvlesshdua
 ;;
-16)
+12)
 cekvlesshdua
+;;
+13)
+addvlessxtls
+;;
+14)
+delvlessxtls
+;;
+15)
+renewvlessxtls
+;;
+16)
+cekvlessxtls
 ;;
 17)
 addvlesshttp
