5a4af437bcc4b6f6f60f65001a68706b34fa3c86
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-07-27 04:37:35 +0700

    Update addvmessquic.sh

--- xray/addvmessquic.sh
+++ xray/addvmessquic.sh
@@ -1,120 +1,118 @@
 #!/bin/bash
-# Created By wisnu cokro satrio
-clear
-
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-
-MYIP=$(wget -qO- ipinfo.io/ip);
+# My Telegram : https://t.me/zerossl
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+#MYIP=$(wget -qO- ipinfo.io/ip);
 MYIP=$(wget -qO- https://ipv4.icanhazip.com);
 MYIP6=$(wget -qO- https://ipv6.icanhazip.com);
 clear
 domain=$(cat /etc/xray/domain)
 vmquic="$(cat ~/log-install.txt | grep -w "VMESS QUIC" | cut -d: -f2|sed 's/ //g')"
-#none="$(cat ~/log-install.txt | grep -w "Vmess None TLS" | cut -d: -f2|sed 's/ //g')"
+nontls="$(cat ~/log-install.txt | grep -w "VMESS WS NON TLS" | cut -d: -f2|sed 's/ //g')"
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
-		read -rp "User: " -e user
+		read -rp "Username : " -e user
 		CLIENT_EXISTS=$(grep -w $user /etc/xray/config.json | wc -l)
 
 		if [[ ${CLIENT_EXISTS} == '1' ]]; then
 			echo ""
-			echo "A Client Username Was Already Created, Please Enter New Username"
+			echo -e "Username ${RED}${CLIENT_NAME}${NC} Already On VPS Please Choose Another"
 			exit 1
 		fi
 	done
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
-		read -rp "User: " -e user
+		read -rp "Username : " -e user
 		CLIENT_EXISTS=$(grep -w $user /etc/xray/xvmess.json | wc -l)
 
 		if [[ ${CLIENT_EXISTS} == '1' ]]; then
 			echo ""
-			echo "A Client Username Was Already Created, Please Enter New Username"
+			echo -e "Username ${RED}${CLIENT_NAME}${NC} Already On VPS Please Choose Another"
 			exit 1
 		fi
 	done
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
-		read -rp "User: " -e user
+		read -rp "Username : " -e user
 		CLIENT_EXISTS=$(grep -w $user /etc/xray/xvless.json | wc -l)
 
 		if [[ ${CLIENT_EXISTS} == '1' ]]; then
 			echo ""
-			echo "A Client Username Was Already Created, Please Enter New Username"
+			echo -e "Username ${RED}${CLIENT_NAME}${NC} Already On VPS Please Choose Another"
 			exit 1
 		fi
 	done
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
-		read -rp "User: " -e user
+		read -rp "Username : " -e user
 		CLIENT_EXISTS=$(grep -w $user /etc/xray/xtrojan.json | wc -l)
 
 		if [[ ${CLIENT_EXISTS} == '1' ]]; then
 			echo ""
-			echo "A Client Username Was Already Created, Please Enter New Username"
+			echo -e "Username ${RED}${CLIENT_NAME}${NC} Already On VPS Please Choose Another"
 			exit 1
 		fi
 	done
 uuid=$(cat /proc/sys/kernel/random/uuid)
-read -p "Expired (Days): " masaaktif
+read -p "Expired (Days) : " masaaktif
 hariini=`date -d "0 days" +"%Y-%m-%d"`
-#read -p "Expired (Seconds) : " masaaktif
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
-#exp2=`date -d "$masaaktif seconds" +"%Y-%m-%d"`
 sed -i '/#vmess-quic$/a\### '"$user $exp"'\
-},{"id": "'""$uuid""'","alterId": '"0"',"email": "'""$user""'"' /etc/xray/xvless.json
+},{"id": "'""$uuid""'"' /etc/xray/xvmess.json
 sed -i '/#vmess-quic$/a\### '"$user $exp"'\
-},{"id": "'""$uuid""'","alterId": '"0"',"email": "'""$user""'"' /etc/xray/xtrojan.json
-sed -i '/#vmess-hdua$/a\### '"$user $exp"'\
-},{"id": "'""$uuid""'","alterId": '"0"',"email": "'""$user""'"' /etc/xray/xvmess.json
-#sed -i '/#none$/a\### '"$user $exp"'\
-#},{"id": "'""$uuid""'","alterId": '"0"',"email": "'""$user""'"' /etc/xray/config.json
+},{"id": "'""$uuid""'"' /etc/xray/xtrojan.json
+#sed -i '/#vmess-nontls$/a\### '"$user $exp"'\
+#},{"id": "'""$uuid""'"' /etc/xray/config.json
 cat>/etc/xray/vmess-$user-tls.json<<EOF
       {
-      "v": "0",
+      "v": "4",
       "ps": "🔰VMESS QUIC TLS ${user}",
-      "add": "${MYIP}",
+      "add": "${domain}",
       "port": "${vmquic}",
       "id": "${uuid}",
       "aid": "0",
       "net": "quic",
-      "path": "/shanumquic",
+      "path": "shanumquic",
       "type": "none",
-      "host": "$domain",
-      "tls": "tls"
+      "host": "${domain}",
+      "tls": "none"
 }
 EOF
-vmessquic=$( base64 -w 0 <<< $vmess_json1)
+vmess_base641=$( base64 -w 0 <<< $vmess_json1)
 #vmess_base642=$( base64 -w 0 <<< $vmess_json2)
 vmessquic="vmess://$(base64 -w 0 /etc/xray/vmess-$user-tls.json)"
-#vmessgrpclink2="vmess://$(base64 -w 0 /etc/xray/$user-none.json)"
+#vmess2="vmess://$(base64 -w 0 /etc/xray/vmess-$user-nontls.json)"
 rm -rf /etc/xray/vmess-$user-tls.json
-rm -rf /etc/xray/vmess-$user-nontls.json
-
-systemctl daemon-reload
-systemctl restart xvless
-systemctl restart xray
+#rm -rf /etc/xray/vmess-$user-nontls.json
+systemctl restart xray.service
 systemctl restart xvmess
+systemctl restart xvless
 systemctl restart xtrojan
 service cron restart
 clear
-echo -e ""
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\033[1;46m   🔰 AKUN VMESS QUIC 🔰  \e[m"   
+echo -e "\033[1;46m   🔰AKUN VMESS QUIC🔰   \e[m"   
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "Nama  :${user}"
-echo -e "Host  :${domain}"
-echo -e "IP  :${MYIP}"
+echo -e "IP/Host  :${MYIP}"
+echo -e "Alamat  :${domain}"
 echo -e "Port TLS  :${vmquic}"
-echo -e "Network  :QUIC"
-echo -e "Host  :${domain}"
+#echo -e "Port No TLS  :${nontls}"
+echo -e "Protokol  :QUIC"
 echo -e "Path  :shanumquic"
-echo -e "Uuid  :${uuid}"
+echo -e "UserID  :${uuid}"
 echo -e "Dibuat  :$hariini"
 echo -e "Kadaluarsa  :$exp"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "VMESS QUIC TLS: ${vmessquic}"   
+echo -e "VMESS QUIC TLS: ${vmessquic}"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-#echo -e "H2C NONTLS: ${vmesshduanon}"   
+#echo -e "VMESS WS NONTLS: ${vmess2}"
 #echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "\033[1;46m🔰LUXURY EDITION ZEROSSL🔰\e[m"   
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e ""
