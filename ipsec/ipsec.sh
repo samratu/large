8c3d27d971ddd13f995db4a91951926f177977d0
THE JAYAKARTA HOTEL AND RESORTS <103863400+samratu@users.noreply.github.com>
2022-07-30 15:49:26 +0700

    Update ipsec.sh

--- ipsec/ipsec.sh
+++ ipsec/ipsec.sh
@@ -2,8 +2,7 @@
 # Debian 9 & 10 64bit
 # Ubuntu 18.04 & 20.04 bit
 # Centos 7 & 8 64bit 
-# Mod By SL
-# SL
+# Mod By gandring
 # ==========================================
 # Color
 RED='\033[0;31m'
